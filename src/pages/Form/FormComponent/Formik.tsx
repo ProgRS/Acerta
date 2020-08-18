78265346ff588a3a00366f49b666cb4e337901c5
Fernando <lfernandotc@hotmail.com>
2020-08-18 18:56:29 -0300

    Versão de entrega

--- src/pages/Form/FormComponent/Formik.tsx
+++ src/pages/Form/FormComponent/Formik.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState, useEffect } from 'react'
 import * as Yup from 'yup'
-import {useFormik} from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { User } from '../../Home'
 import InputMask from 'react-input-mask'
 import api from '../../../services/api'
@@ -8,135 +9,122 @@ import { useHistory, Link } from 'react-router-dom'
 interface FormikProp {
   id?: number
 }
-const Formik: React.FC<FormikProp> = ({id}) => {
-  const [user, setUser] = useState<User | null>(null)
+
+
+const MyFormik: React.FC<FormikProp> = ({ id }) => {
   const history = useHistory()
+  console.log(id)
 
-  useEffect(() => {
-    if(id) {
-      api.get(`/leads/${id}`).then(res => {
-        console.log(res.data);
-        setUser(res.data)
-      })
+  interface ValuesProps {
+    cpf: string
+    email: string
+    estadoCivil: string
+    nome: string
+    nomeConjugue: string
+  }
+
+const onSubmit = async (values: ValuesProps) => {
+  try {
+    console.log(values)
+    if (!id) {
+      await api.post('leads', values)
     }
-  }, [id])  
-  const formik = useFormik({
-    initialValues: {
-      name: user?.nome || "",
-      email: user?.email || "",
-      estadocivil: user?.estadoCivil || "",
-      cpf: user?.cpf || "",
-      nomeconjugue: user?.nomeConjugue || ""
-    },
-    onSubmit: async(values) => {
-      try {
-        const data = {
-          cpf: values.cpf,
-          email: values.email,
-          estadoCivil: values.estadocivil,
-          nome: values.name,
-          nomeConjugue: values.nomeconjugue
-        } as User
-        console.log(data)
-        // if(!id) {
-        //   await api.post('leads', data)
-        // }
-        // else {
-        //   await api.put(`/leads/${id}`, data)
-        // }
-        // history.push('/')
-      } catch(err) {
-        console.log(err)
-      }
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required("Obrigatório"),
-      email: Yup.string().required("Obrigatório"),
-      cpf: Yup.string().required("Obrigatório")
-      // estadocivil: Yup.string().required("Obrigatório")
-    })
-  })
-  
-
+    else {
+      await api.put(`/leads/${id}`, values)
+    }
+    history.push('/')
+  } catch (err) {
+    console.log(err)
+  }
+}
+const initialValues = {
+  nome: "",
+  email: "",
+  estadoCivil: "",
+  cpf: "",
+  nomeConjugue: ""
+}
+const validationSchema = Yup.object({
+  nome: Yup.string().required("Obrigatório"),
+  email: Yup.string().required("Obrigatório").email("Email invalido"),
+  cpf: Yup.string().required("Obrigatório"),
+  estadoCivil: Yup.string().required("Obrigatório")
+})
   return (
-    <form onSubmit={formik.handleSubmit}>
-      {formik.values.estadocivil}
+    <Formik
+      initialValues={
+        initialValues
+      }
+      validationSchema={
+        validationSchema
+      }
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting, setFieldValue, values}) => {
+        const [user, setUser] = useState<any>({ cpf: '', email: '', estadoCivil: '', nome: '', nomeConjugue: '' })
+        useEffect(() => {
+          if (id) {
+            api.get(`/leads/${id}`).then(res => {
+              console.log(res.data);
+              setUser(res.data)
+            })
+          }
+        }, [])
+        useEffect(() => {
+          const fields = Object.keys(user)
+          fields.forEach((field) => setFieldValue((field), user[field], false))
+        }, [setFieldValue, user])
+        return (
+          <Form>
           <div className="input-container">
             <div className="input">
-              <label htmlFor="name">Nome</label>
-              <input
+              <label htmlFor="nome">Nome:</label>
+              <Field
                 type="text"
-                id="name"
-                value={formik.values.name}
-                name="name"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
+                name="nome"
+                id="cpf"
               />
-              {formik.errors.name && formik.touched.name && formik.errors.name}
+              <ErrorMessage component="span" name="nome" />
             </div>
             <div className="input">
-              <label htmlFor="cpf">CPF</label>
-              <InputMask
-                mask="999.999.999-99"
-                value={formik.values.cpf}
-                name="cpf"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              >
-              </InputMask>
-              {formik.errors.cpf && formik.touched.cpf && formik.errors.cpf}
+              <label htmlFor="cpf">CPF:</label>
+              <Field type="text" name="cpf" id="cpf" render={({ field }: any) => (
+                <InputMask mask="999.999.999-99" {...field} />
+              )} />
+              <ErrorMessage name="cpf" component="span" />
             </div>
           </div>
           <div className="input-container">
             <div className="input">
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              value={formik.values.email}
-              name="email"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.errors.email && formik.touched.email && formik.errors.email}
-            </div>      
+              <label htmlFor="email">Email:</label>
+              <Field type="email" name="email" id="email" />
+              <ErrorMessage name="email" component="span" />
+            </div>
             <div className="input">
-              <label htmlFor="estadoCivil">Estado Civil</label>
-              <select
-                name="estadoCivil"
-                // value={formik.values.estadocivil}
-                defaultValue={formik.values.estadocivil}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                id="estadoCivil"
-              >
+              <label htmlFor="estadoCivil">Estado Civil:</label>
+              <Field name="estadoCivil" component={"select"}>
                 <option value="">Selecione uma opcao</option>
                 <option value="Solteiro(a)">Solteiro(a)</option>
                 <option value="Casado(a)">Casado(a)</option>
                 <option value="Viuvo(a)">Viuvo(a)</option>
                 <option value="Separado(a)">Separado(a)</option>
-              </select>
-              {formik.errors.estadocivil && formik.touched.estadocivil && formik.errors.estadocivil}
+              </Field>
+              <ErrorMessage name="estadoCivil" component="span" />
             </div>
           </div>
           <div className="input">
-            <label htmlFor="nomeconjugue">Nome do Conjugue</label>
-            <input
-              type="text"
-              value={formik.values.nomeconjugue}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              id="nomeconjugue"
-              disabled={formik.values.estadocivil !== 'Casado(a)'}
-              className={formik.values.estadocivil !== 'Casado(a)' ? 'disabled' : ''}
-            />
+            <label htmlFor="nomeConjugue">Nome do Conjugue:</label>
+            <Field type="text" name="nomeConjugue" disabled={values.estadoCivil !== "Casado(a)"} id="nomeconjugue" />
           </div>
           <div className="flex">
             <Link className="cancel" to="/">Cancelar</Link>
-            <input type="submit" className="submit" value="Cadastrar"/>
+            <input type="submit" className="submit" disabled={isSubmitting} value="Cadastrar" />
           </div>
-        </form>
+        </Form>
+        )
+      }}
+    </Formik>
   )
 }
 
-export default Formik
+export default MyFormik
